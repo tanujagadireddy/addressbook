1ec1d581b47cf0d29aae4884392c1fb4322fe8bd
devops-trainer <33363806+devops-trainer@users.noreply.github.com>
2021-06-10 07:51:25 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,15 +1,15 @@
 def compile() {
-    echo "compiling the code..."
+   
     sh 'mvn compile'
 } 
 
 def UnitTest() {
-    echo "testing..."
+  
     sh 'mvn test'
 } 
 
 def package() {
-    echo 'packaging the application...'
+ 
   sh 'mvn package'
 } 
 
