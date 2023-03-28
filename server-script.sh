aad0eea9191c6658ced40f64132b0596f83fa9e6
Preethi <preethi@gmail.com>
2023-03-28 07:45:28 +0530

    ansible

--- server-script.sh
+++ server-script.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
-sudo yum install java-1.8.0-openjdk-devel -y
+#sudo yum install java-1.8.0-openjdk-devel -y
+sudo yum install java-1.8.0-amazon-corretto-devel -y
 sudo yum install git -y
 sudo yum install maven -y
 if [ -d "addressbook" ]
