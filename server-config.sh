fa294a5d45bf68026dbe0cb534442a40a947f753
Your Name <you@example.com>
2023-08-16 06:28:30 +0530

    script added

--- server-config.sh
+++ server-config.sh
@@ -5,6 +5,7 @@ sudo yum install maven -y
 if [ -d "addressbook" ]
 then
   echo "repo is already cloned and exists"
+  cd /home/ec2-user/addressbook
   git pull origin develop
 else
    git clone https://github.com/preethid/addressbook.git
