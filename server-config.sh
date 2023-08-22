f947e9f3a296423e054aeea95c4c8fe36b4d13a0
Your Name <you@example.com>
2023-08-22 06:49:41 +0530

    docker stage modified

---
+++ server-config.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+# sudo yum install java-1.8.0-openjdk-devel -y
+# sudo yum install git -y
+# sudo yum install maven -y
+if [ -d "addressbook-v2" ]
+then
+  echo "repo is already cloned and exists"
+  cd /home/ec2-user/addressbook-v2
+  git pull origin cicd-docker
+else
+   git clone https://github.com/preethid/addressbook-v2.git
+fi
+cd /home/ec2-user/addressbook-v2
+sudo yum install docker -y
+sudo systemctl start docker
