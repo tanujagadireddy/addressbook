f37f5dc00319b7a41d1b1b3230a261ec38356f1e
Your Name <you@example.com>
2023-08-22 08:53:31 +0530

    corrected

--- ansible/prepare-ACM.sh
+++ ansible/prepare-ACM.sh
@@ -3,7 +3,7 @@
 pip3 install boto3
 aws configure set aws_access_key_id $1
 aws configure set aws_secret_access_key $2
-ansible-inventory -i /home/ec2-user/inventory_aws_ec2.yml --graph
+/home/ec2-user/.local/bin/ansible-inventory -i /home/ec2-user/inventory_aws_ec2.yml --graph
 chmod 400 /home/ec2-user/.ssh/id_rsa
-ansible-playbook -i /home/ec2-user/inventory_aws_ec2.yml  /home/ec2-user/docker-deploy.yml -e "DOCKER_IMAGE=$4 DOCKER_REG_PASSWORD=$3"
+/home/ec2-user/.local/bin/ansible-playbook -i /home/ec2-user/inventory_aws_ec2.yml  /home/ec2-user/docker-deploy.yml -e "DOCKER_IMAGE=$4 DOCKER_REG_PASSWORD=$3"
 rm /home/ec2-user/.ssh/id_rsa
