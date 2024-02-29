2a2d22140fd9c09f955de6f0064ecfa314858438
Preethi Dovala <preethidovala@Preethis-MacBook-Pro.local>
2024-02-29 08:22:11 +0530

    ansible added

--- server-script.sh
+++ server-script.sh
@@ -1,16 +1,21 @@
-sudo yum  install java-1.8.0-openjdk-devel -y
+#! /bin/bash
+
+#sudo yum install java-1.8.0-openjdk-devel -y
 sudo yum install git -y
-sudo yum install maven -y
+#sudo yum install maven -y
+sudo yum install docker -y
+sudo systemctl start docker
+
 if [ -d "addressbook" ]
-then 
-  echo "repo is cloned and exists"
-  cd /home/ec2-user/addressbook
-  git pull origin k8s-cicd
+then
+   echo "repo is already cloned and exists"
+   cd /home/ec2-user/addressbook
+   git pull origin master
 else
-  echo "repo is not there"
- git clone https://github.com/preethid/addressbook.git
- cd /home/ec2-user/addressbook
+   git clone https://github.com/preethid/addressbook.git
+   cd addressbook
+   git checkout master
 fi
- mvn package
- sudo yum install docker -y
- sudo systemctl start docker
+cd addressbook
+#mvn package
+sudo docker build -t $1 /home/ec2-user/addressbook
