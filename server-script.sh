d1d7be1befdcd6a501f6d43e8ff0202f59d11ee1
Preethi Dovala <preethidovala@Preethis-MacBook-Pro.local>
2024-03-28 06:53:14 +0530

    ansible stage

--- server-script.sh
+++ server-script.sh
@@ -1,18 +1,19 @@
-#! /bin/bash
-#sudo yum install java-1.8.0-openjdk-devel -y
-#sudo yum install java-1.8.0-amazon-corretto-devel -y
-sudo yum install java-1.8.0-openjdk -y
 sudo yum install git -y
-sudo yum install maven -y
+#sudo yum install java-1.8.0-openjdk-devel -y
+#sudo yum install maven -y
+sudo yum install docker -y
+sudo systemctl start docker
+
 if [ -d "addressbook" ]
-then 
+then
   echo "repo is cloned and exists"
   cd /home/ec2-user/addressbook
   git pull origin ANSIBLE_CICD
 else
-  git clone https://github.com/preethid/addressbook.git
+  git clone  https://github.com/preethid/addressbook.git
+  cd addressbook
+  git checkout ANSIBLE_CICD
 fi
-cd /home/ec2-user/addressbook
-mvn package
-sudo yum install docker -y
-sudo systemctl start docker
+
+sudo docker build -t $1:$2 /home/ec2-user/addressbook
+#devopstrainer/java-mvn-privaterepos:1
